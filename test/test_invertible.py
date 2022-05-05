ee36a519b62162aca768f02c83a068710e56204e
zzp1012 <zzp1012@sjtu.edu.cn>
2022-05-05 12:53:42 +0800

    update: minor

--- test/test_invertible.py
+++ test/test_invertible.py
@@ -1,28 +1,27 @@
 import os, sys
 import torch
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt 
-import seaborn as sns
 from torch.utils.data import DataLoader
-from sklearn.manifold import TSNE
+
+# Changable configs
+MODEL_PATH = "NEED TO BE FILLED"
 
 # import internal libs
-sys.path.insert(0, "/data/zzp1012/realNVP/src")
+sys.path.insert(0, os.path.join(os.path.dirname(MODEL_PATH), "../src/"))
 from data import load_data
 from data.utils import logit_transform
 from model import build_model
 
 DATASET = "mnist"
-DEVICE = torch.device("cpu")
+DEVICE = torch.device("cuda:0")
 BASE_DIM = 64
 RES_BLOCKS = 8
 BOTTLENECK = 0
 SKIP = 1
 WEIGHT_NORM = 1
-COUPLING_BN = 0
+COUPLING_BN = 1
 AFFINE = 1
 BATCH_SIZE = 64
+BN_TYPE = "ln"
 
 # load data
 _, val_split, data_info = load_data(dataset = DATASET)
@@ -38,11 +37,14 @@ flow = build_model(device = DEVICE,
                     skip = SKIP,
                     weight_norm = WEIGHT_NORM,
                     coupling_bn = COUPLING_BN,
-                    affine = AFFINE)
+                    affine = AFFINE,
+                    bn_type = BN_TYPE)
 print(flow)
+flow.load_state_dict(torch.load(MODEL_PATH))
 flow = flow.double()
 
 
+flow.()
 for batch_idx, data_ in enumerate(val_loader, 1):
     x, lbls = data_
     # log-determinant of Jacobian from the logit transform
