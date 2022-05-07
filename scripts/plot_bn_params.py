fb7b81ca466267dd3d7647ac4bbf2820697103bd
zzp1012 <zzp1012@sjtu.edu.cn>
2022-05-07 08:26:56 +0800

    update: minor update

--- scripts/plot_bn_params.py
+++ scripts/plot_bn_params.py
@@ -2,6 +2,7 @@ import os
 import torch
 import numpy as np
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 def plot_multiple_curves(save_path: str, 
                          res_dict: dict,
@@ -17,7 +18,7 @@ def plot_multiple_curves(save_path: str,
     # Draw all the lines in the same plot, assigning a label for each one to be
     # shown in the legend.
     for key in res_dict.keys():
-        ax.plot(np.arange(len(res_dict[key])) + 1, np.asarray(res_dict[key]), label=key)
+        ax.plot(range(100, epochs + 1, 100), np.asarray(res_dict[key]), label=key)
     ax.grid()
     ax.set(xlabel = 'epoch', title = name)
     # Add a legend, and position it on the lower right (with no box)
@@ -27,24 +28,30 @@ def plot_multiple_curves(save_path: str,
     fig.savefig(path)
     plt.close()
 
-res_path = "NEED TO BE FILLED"
+res_path = "/data/zzp1012/realNVP/outs/tmp/0505-220330-mnist-seed0-blocks8-bottle0-ln-label-lr0.001-bs64-wd0.0/train"
 plot_dim = 15
-epochs = 100
+epochs = 5000
 
-for dir_name in os.listdir(res_path):
-    for postfix in ["mean", "var"]:
-        val_lst = []
-        for i in range(1, epochs+1):
-            val = torch.load(os.path.join(res_path, dir_name, f"{i}_{postfix}.pth"))
-            val_lst.append(val)
-        val_lst = torch.stack(val_lst).cpu().numpy()
-        N, D = val_lst.shape
-        assert N == epochs, "the number of epochs is not correct"
+para_names = [
+    "s1_ckbd.0.coupling.out_bn.running_mean",
+    "s1_ckbd.0.coupling.out_bn.running_var",
+    "s1_chan.0.coupling.out_bn.running_mean",
+    "s1_chan.0.coupling.out_bn.running_var",
+    "s2_ckbd.0.coupling.out_bn.running_mean",
+    "s2_ckbd.0.coupling.out_bn.running_var",
+]
 
-        val_dict = {f"dim{i}": val_lst[:, i] for i in range(min(D, plot_dim))}
+for pname in tqdm(para_names):
+    val_lst = []
+    for epoch in range(100, epochs + 1, 100):
+        model_state = torch.load(os.path.join(res_path, "model_epoch{}.pt".format(epoch)))
+        val_lst.append(model_state[pname].cpu().numpy())
+    val_lst = np.asarray(val_lst)
+    N, D = val_lst.shape
+    val_dict = {f"dim{i}": val_lst[:, i] for i in range(min(D, plot_dim))}
 
-        save_path = os.path.join(res_path, dir_name)
-        os.makedirs(save_path, exist_ok=True)
-        plot_multiple_curves(save_path = save_path,
-                             res_dict = val_dict,
-                             name = f"{postfix}_curve")
+    save_path = os.path.join(res_path, "bn_params")
+    os.makedirs(save_path, exist_ok=True)
+    plot_multiple_curves(save_path = save_path,
+                         res_dict = val_dict,
+                         name = f"{pname}_curve")
